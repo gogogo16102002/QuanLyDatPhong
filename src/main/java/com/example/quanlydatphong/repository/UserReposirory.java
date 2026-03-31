fedc4777a92cfa67c0dc5ab52e8098a06726cd4f
gogogo16102002 <gogogo16102002@gmail.com>
2026-04-01 03:28:15 +0700

    tao repository

--- src/main/java/com/example/quanlydatphong/repository/UserReposirory.java
+++ src/main/java/com/example/quanlydatphong/repository/UserReposirory.java
@@ -4,7 +4,12 @@ import com.example.quanlydatphong.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserReposirory extends JpaRepository<User,Long> {
 
+    Optional<User> findByUsername(String username);
+
+    boolean existsByUsername(String username);
 }
