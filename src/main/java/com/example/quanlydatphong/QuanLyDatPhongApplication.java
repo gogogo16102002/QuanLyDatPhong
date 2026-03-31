39335d0a058c6ae63c88189fcf68397fca775742
Admin <gogogo16102002@gmail.com>
2026-04-01 02:18:18 +0700

    tao

---
+++ src/main/java/com/example/quanlydatphong/QuanLyDatPhongApplication.java
@@ -0,0 +1,13 @@
+package com.example.quanlydatphong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanLyDatPhongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuanLyDatPhongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/quanlydatphong/QuanLyDatPhongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quanlydatphong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanLyDatPhongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
